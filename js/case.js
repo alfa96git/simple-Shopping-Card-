f38e168a91bb64b8f20a6f75b1d0d4102cbbd661
alfa96git <alfa.sunny.96@gmail.com>
2023-02-17 01:02:05 +0600

    Added Case Field Button Section

--- js/case.js
+++ js/case.js
@@ -1,14 +1,3 @@
-function getCaseFieldNumberById(caseValue){
-    const caseFieldNumber = document.getElementById(caseValue);
-    const caseFieldNumberString = caseFieldNumber.value;
-    const previousCaseFieldNumber = parseInt(caseFieldNumberString);
-
-    const newCaseFieldNumber = previousCaseFieldNumber + 1;
-     caseFieldNumber.value = newCaseFieldNumber;
-}
-
-
-
 
 
 
@@ -24,22 +13,18 @@ document.getElementById('btn-case-plus').addEventListener('click', function () {
     
     caseField.value = fieldValueTotal;
 
-
-    // getCaseFieldNumberById('case-number-field');
 });
 
 
 
 // Calculate Minus Button
 document.getElementById('btn-case-minus').addEventListener('click', function () {
+    const caseField = document.getElementById('case-number-field');
+    const caseFieldString = caseField.value;
+    const previousFieldValue = parseInt(caseFieldString);
 
-    // const caseFieldNumber = document.getElementById('case-number-field');
-    // const caseFieldNumberString = caseFieldNumber.value;
-    // const previousFieldNumberValue = parseInt(caseFieldNumberString);
+    const fieldValueTotal = previousFieldValue - 1;
 
-    // const newCaseFieldValueNumber = previousFieldNumberValue - 1;
+    caseField.value = fieldValueTotal;
 
-    // caseFieldNumber.value = newCaseFieldValueNumber;
-
-    getCaseFieldNumberById('case-number-field');
 });
